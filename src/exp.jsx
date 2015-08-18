cc70ca7b2fc8f3640c5fdcd315c251cbc51bd920
IveWang <Ivework@outlook.com>
2015-08-18 17:51:09 +0800

    test

--- src/exp.jsx
+++ src/exp.jsx
@@ -1,29 +1,29 @@
-// let React = require('react');
+import React from 'react';
 
-// let exp = React.createClass({
-// 	getStyles(){
-// 		return {
-// 			root: {
-// 				padding: 8
-// 			}
-// 		};
-// 	},
+let exp = React.createClass({
+	getStyles(){
+		return {
+			root: {
+				padding: 8
+			}
+		};
+	},
 
-// 	render(){
-// 		let style = this.getStyles();
+	render(){
+		let style = this.getStyles();
 
-// 		let children = React.Children.map(this.props.children, (child) => {
-// 			return React.cloneElement(child, {
-// 				style: {marginRight: 8}
-// 			});
-// 		});
+		let children = React.Children.map(this.props.children, (child) => {
+			return React.cloneElement(child, {
+				style: {marginRight: 8}
+			});
+		});
 
-// 		return {
-// 			<div {...this.props} style={style.root}>
-// 				{children}
-// 			</div>
-// 		};
-// 	}
-// });
+		return (
+			<div {...this.props} style={style.root}>
+				{children}
+			</div>
+		);
+	}
+});
 
-// module.exports = exp;
+export default exp;
